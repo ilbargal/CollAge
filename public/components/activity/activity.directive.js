2e377c5e3d30483963e5a48908e934549cf370fc
DanielFedo <thecrow34@gmail.com>
2016-05-14 19:42:21 +0300

    Fixed calendar tooltip and changed the event directive to work well with the calendar

--- public/components/activity/activity.directive.js
+++ public/components/activity/activity.directive.js
@@ -1,11 +1,17 @@
 (function () {
     var eventService = function() {
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
         var data = [
             {
                 id:1,
                 title:'רכיבה על אופניים',
                 description: 'רכיבה שווה על אופניים. מומלצת לבעלי כושר גופני גבוה',
-                datetime: '14/04/2016',
+                start: new Date(y, m, d - 5),
+                end: new Date(y, m, d - 2),
                 location: 'רמת גן',
                 imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Left_side_of_Flying_Pigeon.jpg'
             },
@@ -13,7 +19,8 @@
                 id:2,
                 title:'שחייה צורנית',
                 description: 'שחיה בספורטן העירוני בשלל צורות מעניינות. דרושים משתתפים רבים',
-                datetime: '15/04/2016',
+                start: '15/04/2016',
+                end: '',
                 location: 'תל אביב',
                 imageSrc: 'http://images.mouse.co.il/storage/8/f/490_fake_gay.jpg'
             },
@@ -21,15 +28,17 @@
                 id:3,
                 title:'ארוחת צהריים בארומה',
                 title:'פגישה לקפה ומאפה בסניף הוד השרון',
-                datetime: '20/04/2016',
+                start: '20/04/2016',
+                end: '',
                 location: 'הוד השרון',
                 imageSrc: 'http://www.aroma.co.il/_media/images/general/logo.png'
             },
             {
                 id:4,
-                title:'ערב קלפים מטורף',
+                start:'ערב קלפים מטורף',
                 description: 'ברידג, פוקר ועוד',
-                datetime: '23/04/2016',
+                start: '23/04/2016',
+                end: '',
                 location: 'כפר סבא, רחוב השופטים 26ב',
                 imageSrc: 'http://www.best-games-ever.com/files/images/32.JPG'
             },
@@ -57,14 +66,15 @@
                         id: '@',
                         title: '@',
                         location: '@',
-                        datetime: '@',
+                        start: '@',
+                        end: '@',
                      },
                     replace: true,
                     templateUrl: 'components/activity/activity.html',
                     controller: ['$scope', 'eventService', function($scope, eventService) {
                         var activityFromDB = eventService.getEventById($scope.id);
                         $scope.location = activityFromDB.location;
-                        $scope.datetime = activityFromDB.datetime;
+                        $scope.start = activityFromDB.start;
                         $scope.imageSrc = activityFromDB.imageSrc;
                     }],
                     link: function ($scope, element, attrs) { } //DOM manipulation
