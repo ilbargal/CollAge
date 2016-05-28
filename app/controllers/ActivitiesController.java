665a980843ebcac5b24f81332e9b4a8b64de85fd
Bar Gal <ilbargal@gmail.com>
2016-05-28 22:43:06 +0300

    My CHanges

--- app/controllers/ActivitiesController.java
+++ app/controllers/ActivitiesController.java
@@ -27,4 +27,9 @@ public class ActivitiesController extends Controller {
 
         return ok(Utils.convertObjectToJsonString(evt));
     }
+
+    public Result addActivity() {
+        System.out.println(request().body().asJson().toString());
+        return ok();
+    }
 }
